65ff468fc3161370868b6e3ba6ce358e84254fce
Vikram Tiwari <Vikramtheone1@gmail.com>
2015-01-08 14:10:48 +0530

    consolidated data

--- geo.js
+++ geo.js
@@ -4,13 +4,24 @@ var mmdbreader = require('maxmind-db-reader');
 var city = mmdbreader.openSync('./city.mmdb');
 
 // get geodata
-var geodata = city.getGeoDataSync('128.101.101.101');
+var geodata = city.getGeoDataSync('182.71.185.139');
 
 // form output
-var result = {
-    'City': geodata.city.names.en,
-    'Country': geodata.country.names.en
+var city = '',
+    country = '';
+
+// consolidate data
+if (geodata.cities == undefined || typeof geodata.cities === undefined) {
+    city = 'NA'
+} else {
+    city = geodata.cities.names.en;
+}
+
+if (geodata.country == undefined || typeof geodata.country === undefined) {
+    country = 'NA'
+} else {
+    country = geodata.country.names.en;
 }
 
 // output
-console.log(result);
+console.log(city, country);
