fe030fc1c97ff241c7e43a969f24987b8857360e
Vikram Tiwari <Vikramtheone1@gmail.com>
2015-01-08 13:51:33 +0530

    using singular file for city and country

--- geo.js
+++ geo.js
@@ -2,20 +2,13 @@ var mmdbreader = require('maxmind-db-reader');
 
 // get databases files from http://dev.maxmind.com/geoip/geoip2/geolite2/ and rename them to city and country respectively
 
-// country database
-mmdbreader.open('./country.mmdb',function(err,countries){
-    // get geodata
-    countries.getGeoData('128.101.101.101',function(err,geodata){
-        // log data
-        console.log(geodata);
-    });
-});
-
 // city database
-mmdbreader.open('./city.mmdb',function(err,cities){
-    // get geodata
-    cities.getGeoData('128.101.101.101',function(err,geodata){
-        // log data
-        console.log(geodata);
+mmdbreader.open('./city.mmdb', function(err, cities) {
+    cities.getGeoData('128.101.101.101', function(err, geodata) {
+        var result = {
+            city: geodata.city.names.en,
+            country: geodata.country.names.en
+        }
+        console.log(result);
     });
 });
