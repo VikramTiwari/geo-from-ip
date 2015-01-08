7600de14b80c5c0c5d4d7292f51923b51cc720cf
Vikram Tiwari <Vikramtheone1@gmail.com>
2015-01-08 13:14:12 +0530

    city and country
    :P

---
+++ geo.js
@@ -0,0 +1,21 @@
+var mmdbreader = require('maxmind-db-reader');
+
+// get databases files from http://dev.maxmind.com/geoip/geoip2/geolite2/
+
+// country database
+mmdbreader.open('./country.mmdb',function(err,countries){
+    // get geodata
+    countries.getGeoData('128.101.101.101',function(err,geodata){
+        // log data
+        console.log(geodata);
+    });
+});
+
+// city database
+mmdbreader.open('./city.mmdb',function(err,cities){
+    // get geodata
+    cities.getGeoData('128.101.101.101',function(err,geodata){
+        // log data
+        console.log(geodata);
+    });
+});
