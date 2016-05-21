8291fb90815c1c871bb8207c9ffc5b5966d5af5c
Vikram Tiwari <vikramtheone1@gmail.com>
2016-05-21 13:53:44 -0700

    removed mmdb file and added db update in post install

--- mmdb/config.js
+++ mmdb/config.js
@@ -1,19 +1,13 @@
 'use strict';
 
 // where your GeoIP databases are stored
-exports.dbDir = __dirname + '/../mmdb/';
-
-// the download site to fetch them from
-exports.hostName = 'geolite.maxmind.com';
-exports.hostPort = 80;
-exports.urlPath = '/download/geoip/database/';
-exports.userAgent = 'MaxMind-geolite-mirror-simple/0.0.3';
+exports.dbDir = __dirname + '/';
 
 //local-filename, geolite-name
 exports.geoIpDbs = [{
 	local: 'GeoLite2-City.mmdb',
-	remote: 'GeoLite2-City.mmdb.gz'
+	remote: 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
 }, {
 	local: 'GeoLite2-Country.mmdb',
-	remote: 'GeoLite2-Country.mmdb.gz'
+	remote: 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz'
 }, ];
--- mmdb/fileslist.md
+++ mmdb/fileslist.md
@@ -1,9 +1,9 @@
+## Free Databases [https://dev.maxmind.com/geoip/geoip2/geolite2/]
+
+> GeoLite2-City.mmdb [https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz]
+> GeoLite2-Country.mmdb [https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz]
+
 ## Paid databases [https://www.maxmind.com/en/geoip2-databases]
 
 > GeoIP2-City.mmdb
 > GeoIP2-ISP.mmdb
-
-## Free Databases [http://dev.maxmind.com/geoip/geoip2/geolite2/]
-
-> GeoLite2-City.mmdb [http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz]
-> GeoLite2-Country.mmdb [http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz]
