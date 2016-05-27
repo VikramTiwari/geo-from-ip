45f5d94129ee45c673d94955b67159ae5a909e28
Vikram Tiwari <vikramtheone1@gmail.com>
2016-05-27 10:21:26 -0700

    removed country db file, using city only

--- mmdb/config.js
+++ mmdb/config.js
@@ -3,11 +3,8 @@
 // where your GeoIP databases are stored
 exports.dbDir = __dirname + '/';
 
-//local-filename, geolite-name
+//local-filename, remote geolite-url
 exports.geoIpDbs = [{
-	local: 'GeoLite2-City.mmdb',
-	remote: 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
-}, {
-	local: 'GeoLite2-Country.mmdb',
-	remote: 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz'
-}, ];
+  local: 'GeoLite2-City.mmdb',
+  remote: 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
+}];
