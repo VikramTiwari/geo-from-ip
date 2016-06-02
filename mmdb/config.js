571125c8738e4a20d2a93f95337486c49069e066
Vikram Tiwari <vikramtheone1@gmail.com>
2016-06-01 20:05:54 -0700

    semistandard, lower nodejs, LTS and current builds

--- mmdb/config.js
+++ mmdb/config.js
@@ -1,9 +1,11 @@
 'use strict';
 
-// where your GeoIP databases are stored
-exports.dbDir = __dirname + '/';
+let path = require('path');
 
-//local-filename, remote geolite-url
+// where your GeoIP databases are stored
+exports.dbDir = path.resolve(path.join(__dirname, '/'));
+
+// local-filename, remote geolite-url
 exports.geoIpDbs = [{
   local: 'GeoLite2-City.mmdb',
   remote: 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
--- test/test.js
+++ test/test.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var expect = require('chai').expect,
-  Chance = require('chance'),
-  chance = new Chance(),
-  geo = require('../lib/geo-from-ip.js');
+let expect = require('chai').expect;
+let Chance = require('chance');
+let chance = new Chance();
+let geo = require('../lib/geo-from-ip.js');
 
-describe('Geos', function() {
+describe('Geos', function () {
   var ip = chance.ip();
   var result = geo.allData(ip);
 
   if (result.error) {
-    describe('error()', function() {
-      it('should return error message and IP', function() {
+    describe('error()', function () {
+      it('should return error message and IP', function () {
         expect(result).to.have.a.property('ip');
         expect(result).to.have.a.property('error');
       });
     });
   } else {
-    describe('allData()', function() {
-      it('should return city level information', function() {
+    describe('allData()', function () {
+      it('should return city level information', function () {
         expect(result).to.have.a.property('city');
       });
-      it('should return state level information', function() {
+      it('should return state level information', function () {
         expect(result).to.have.a.property('state');
       });
-      it('should return country level information', function() {
+      it('should return country level information', function () {
         expect(result).to.have.a.property('country');
       });
-      it('should return continent level information', function() {
+      it('should return continent level information', function () {
         expect(result).to.have.a.property('continent');
       });
-      it('should return location information', function() {
+      it('should return location information', function () {
         expect(result).to.have.a.property('location');
       });
     });
