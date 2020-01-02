c324b82fc9e1986c4c2b9637046184c138139848
Vikram Tiwari <vikramtheone1@gmail.com>
2020-01-02 09:38:21 -0800

    fix: uncompress and cleanup files

--- mmdb/config.js
+++ mmdb/config.js
@@ -6,5 +6,5 @@ exports.dbDir = __dirname
 // local:filename, remote:geolite-url
 exports.geoIpDbs = [{
   filename: 'GeoLite2-City',
-  remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=mmdb`
+  remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=tar.gz`
 }]
