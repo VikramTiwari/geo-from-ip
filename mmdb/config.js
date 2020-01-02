8df8fee1b6f989ee886f775a70ed1f9c03a1277b
Vikram Tiwari <vikramtheone1@gmail.com>
2020-01-01 23:16:46 -0800

    WIP: download zipped file

--- mmdb/config.js
+++ mmdb/config.js
@@ -5,6 +5,6 @@ exports.dbDir = __dirname
 
 // local:filename, remote:geolite-url
 exports.geoIpDbs = [{
-  local: 'GeoLite2-City.mmdb',
-  remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=tar.gz`
+  filename: 'GeoLite2-City',
+  remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=mmdb`
 }]
